99179d6281eef2d2b25a5d66851f2cd4876378c1
isaacvandor <isaacvandor@gmail.com>
2018-03-02 14:30:28 -0500

    Added model class implementation

--- brickbreaker.py
+++ brickbreaker.py
@@ -15,11 +15,39 @@ import pygame
 from pygame.locals import *
 import time
 
+class BrickBreakerModel(object):
+    """Encodes a brick breaker model"""
+    def __init__(self):
+        self.bricks = []
+        self.bricks.append(Brick(50,100,20,20))
+        self.bricks.append(Brick(50,100,40,20))
+
+    def __str__(self):
+        output_lines = []
+        # convert each brick to a string for outputting
+        for brick in self.bricks:
+            output_lines.append(str(brick))
+        # Print one brick per line
+        return "\n".join(output_lines)
+
+class Brick(object):
+    """encodes the brick into the game"""
+    def __init__(self,height,width,x,y):
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "Brick height=%f, width=%f, x=%f, y=%f" %(self.height,self.width,self.x,self.y)
+
 if __name__ == '__main__':
     pygame.init()
 
     size = (640, 480)
     screen = pygame.display.set_mode(size)
+
+    model = BrickBreakerModel()
+    print(model)
 
     running = True
     while running:
